59269f3a4d24f497bc56ac130596d268793bebf7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-29 13:51:58 +0000

    Deploy signalwerk/React.Spritz to github.com/signalwerk/React.Spritz.git:gh-pages

---
+++ precache-manifest.b3309a61656a85b27c052954d174b604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e551dd0e404c52c2631e",
+    "url": "./static/css/main.d412e065.chunk.css"
+  },
+  {
+    "revision": "e551dd0e404c52c2631e",
+    "url": "./static/js/main.e551dd0e.chunk.js"
+  },
+  {
+    "revision": "3e2b984b01206c672df0",
+    "url": "./static/css/1.4361b178.chunk.css"
+  },
+  {
+    "revision": "3e2b984b01206c672df0",
+    "url": "./static/js/1.3e2b984b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9fc1ba13b1040a28c8723985b7ec704f",
+    "url": "./index.html"
+  }
+];
